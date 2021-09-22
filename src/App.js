28c57ae5e879cbc77ceaaecb6202c2ab4e8d10d7
Manuel <manuelalejandrohernandezmarin@gmail.com>
2021-09-21 22:49:41 -0500

    Finishing pagination

--- src/App.js
+++ src/App.js
@@ -8,9 +8,27 @@ import Paginacion from './Paginacion';
 function App() {
 
 	const [paginaActual, setPaginaActual ] = useState(1);
+	const TOTAL_POR_PAGINA = 7;
 
 	let movies = MoviesJSON;
-  return(
+
+	const cargarPeliculas = () =>{
+		movies = movies.slice(
+		(paginaActual - 1) * TOTAL_POR_PAGINA, 
+		paginaActual * TOTAL_POR_PAGINA
+		);
+	}
+	
+
+	const getTotalPaginas = () => {
+		let cantidadTotalPeliculas = MoviesJSON.length;
+		return Math.ceil(cantidadTotalPeliculas / TOTAL_POR_PAGINA);
+	}
+
+  
+	cargarPeliculas();
+  
+	return(
 	<PageWrapper>
 		{movies.map(movie => 
 			<Pelicula titulo={movie.titulo} 
@@ -25,7 +43,7 @@ function App() {
 			</Pelicula>
 		)}
 
-		<Paginacion pagina = {paginaActual} total = {4} onChange={(pagina) => {
+		<Paginacion pagina = {paginaActual} total = {getTotalPaginas()} onChange={(pagina) => {
 			setPaginaActual(pagina);
 		}}/>
 	</PageWrapper>
